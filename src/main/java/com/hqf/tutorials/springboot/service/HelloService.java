c87fc6986ae12e841a5171569da76a70e461eaf2
Quanfu Huo <qf.huo@outlook.com>
2016-05-25 19:01:06 +0800

    add JunitTest packages

---
+++ src/main/java/com/hqf/tutorials/springboot/service/HelloService.java
@@ -0,0 +1,15 @@
+package com.hqf.tutorials.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by huoquanfu on 2016/5/25.
+ */
+@Service
+public class HelloService {
+
+    public String getName() {
+        return "hello";
+    }
+
+}
